209dad5ca7ef13e27a67a4c84f22ec11fa796190
Helio Borges <helioacruzfilho@gmail.com>
2025-02-17 23:40:48 -0300

    Tela de login procuração

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="login">
+
+            <div class="login-sixdevs">SixDevs</div>
+
+            <form action="">
+
+                <div>
+                    <div class="login-info">
+                        <label class="login-user" for="">Usuário ou Email</label>
+                    </div>
+                    <div class="login-access">
+                        <input class="user" type="text" require>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="login-info">
+                        <label class="login-passwor" for="">Senha</label>
+                    </div>
+                    <div class="login-access">
+                        <input class="password" type="text" require>
+                    </div>
+                </div>
+
+                <div class="change-password">Esqueci a minha senha</div>
+
+                <button class="button-enter">Entrar</button>                
+
+            </form>
+
+            <button class="button-register">Registrar-me</button>             
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
