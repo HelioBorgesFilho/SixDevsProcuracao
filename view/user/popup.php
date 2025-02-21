307753312486ad0d63d2fc91e066b486b7ce97ce
Helio Borges <helioacruzfilho@gmail.com>
2025-02-20 22:46:32 -0300

    Pop-up de informações do perfil

---
+++ view/user/popup.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style.css">
+</head>
+
+<body class="popup">
+
+    <div class="container-popup">
+
+        <div class="info-popup">
+
+            <div class="user">
+
+                <div class="img">
+
+                    <img class="return" src="../../public/img/right-from-bracket-solid.svg" alt="">
+
+                </div>
+
+                <div class="info">
+
+                    <img class="user-img" src="../../public/img/circle-user-solid.svg" alt="">
+
+                    <div class="data">
+                        <span class="name-user">
+                            <p>Helio Borges</p>
+                        </span>
+
+                        <div class="email">
+                            <p>helioborgesdacruz@gmail.com</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="user-info">
+
+                <div class="img">
+
+                    <img class="edit" src="../../public/img/pen-to-square-solid.svg" alt="">
+
+                </div>
+
+                <div class="name">
+                    <label for="">Nome usuário</label>
+                    <p>Helio Borges</p>
+                </div>
+                <div class="full-name">
+
+                    <span class="first-name">
+                        <label for="">Nome</label>
+                        <p>Helio</p>
+                    </span>
+                    <span class="surname">
+                        <label for="">Sobrenome</label>
+                        <p>Borges da Cruz Filho</p>
+                    </span>
+
+                </div>
+
+                <div class="email">
+                    <label for="">Email</label>
+                    <p>helioborgesdacruz@gmail.com</p>
+                </div>
+
+                <div class="edit-password">
+                    <p>Alterar minha senha</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
